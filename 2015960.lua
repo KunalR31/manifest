8be99a5e292005565a59014625d8789730744f61
MANGO UNLOCKER <noreply@steamden.gg>
2025-04-19 00:36:47 -0500

    .gg/steamden

---
+++ 2015960.lua
@@ -0,0 +1,7 @@
+addappid(2015960)
+addappid(2015961, 5424181040, "49e28894827dbb154e036251a43241f0b5c9aa01cf1f3d09db9ddcaed8e526fb")
+setManifestid(2015961, "3286795613354607404", 12542668644)
+
+-- All DLC's already processed!
+
+-- Game ~ Parcel Corps
