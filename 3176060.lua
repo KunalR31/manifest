58f986887cdaae86d4384ad8fc6d57165ed24484
MANGO UNLOCKER <noreply@steamden.gg>
2025-05-04 14:47:39 -0500

    .gg/steamden

---
+++ 3176060.lua
@@ -0,0 +1,10 @@
+-- This lua file has been fetched from the bot VICTOR which is the exclusive property of Piracy Lords server.
+-- Redistribution of VICTORs files is not allowed AT ALL
+-- Join the offical here: https://discord.gg/piracylords
+
+addappid(3176060)
+addappid(228989)
+addappid(228990)
+setManifestid(228990,"1829726630299308803")
+addappid(3176061,0,"87ccd43a7ff83224384228a50111bc6cfee10144e93af2284c27dcdae12d5e38")
+setManifestid(3176061,"8934458582853199597")
