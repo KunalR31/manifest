7b0e0cbdf8cfaf95d6887091826ee684cc319d5a
MANGO UNLOCKER <noreply@steamden.gg>
2025-07-17 01:40:01 -0500

    .gg/steamden

---
+++ 3241660.lua
@@ -0,0 +1,13 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 3241660 Manifest
+-- Name: R.E.P.O.
+-- Generated: 2025-07-13 14:57:36
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3241660) -- R.E.P.O.
+
+-- MAIN APP DEPOTS
+addappid(3241661, 1, "0a0fcffb2de6fb0fa6caff076a5a8cb960903d17320968d5c042fd5a4f9ad2ad") -- Depot 3241661
+setManifestid(3241661, "4154963422191387469", 0)
