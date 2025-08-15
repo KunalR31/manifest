60a0e1b6497a68a0dfd1de3712e1cc8a13a2bc79
MANGO UNLOCKER <actions@github.com>
2025-08-15 01:32:53 +0000

    Remove setManifestid lines from 1881700.lua

--- 1881700.lua
+++ 1881700.lua
@@ -1,8 +1,6 @@
 addappid(1881700)
 addappid(228989)
-setManifestid(228989,"550968249685141759")
 addappid(228990)
-setManifestid(228990,"1829726630299308803")
 addappid(1881701)
 addappid(1881702)
 addappid(1881703)
